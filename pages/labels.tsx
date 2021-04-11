6a268d40a08fce22726c8fb6bb645467fca31a6e
Ade Adeola <adedotster@gmail.com>
2021-04-11 13:50:12 +0100

    remove reliance on getting user info

--- pages/labels.tsx
+++ pages/labels.tsx
@@ -3,13 +3,12 @@ import AppContainer from "../container/AppContainer";
 import Layout from "../container/Layout";
 import TaskCollection from "../container/TaskCollection";
 import { useTaskList } from "../context/taskListContext";
-import { useUserInfo } from "../context/userInfoContext";
 import toTitleCase from "../scripts/toTitleCase";
 
 // in case someone created "unlabelled" label
 const unlabelled = "egj9b39bn2217352b06a6178c0e95f431f30ej5ra";
 
-const getLabelCollection = (labels: string[], taskList: any[]) => {
+const getLabelCollection = (taskList: any[]) => {
   console.log(taskList.length);
   const tempCollection = {
     [unlabelled]: [],
@@ -29,10 +28,8 @@ const getLabelCollection = (labels: string[], taskList: any[]) => {
 };
 
 export default function Labels() {
-  const userInfo = useUserInfo();
   const taskList = useTaskList();
-  const { labels } = userInfo;
-  const labelCollection = getLabelCollection(labels, taskList);
+  const labelCollection = getLabelCollection(taskList);
   
   return (
     <Layout>
